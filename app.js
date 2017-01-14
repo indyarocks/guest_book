1994618b3655929c902fbe6deed3ebb55953c8ff
Chandan Kumar <chandan.jhun@gmail.com>
2017-01-14 10:24:30 +0530

    minor logger msg changed

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ var app = express();
 // New middleware
 app.use(function(req, res, next){
 	console.log('My fun middleware: start!')
-	console.log(req);
-	console.log(res);
+	console.log("Here comes a request " + req.method + " to " + req.url);
 	console.log('My fun middleware: end!')
 	next();
 });
@@ -15,7 +14,7 @@ app.use(function(req, res, next){
 app.use(function(req, res){
 	console.log('Here comes request at: ' + req.url);
 	res.writeHead(200, { "Content-Type": "text/plain"})
-	res.end('Here comes request at: ' + req.url);
+	res.end("Here comes a request " + req.method + " to " + req.url);
 	// res.status(200).send('Here comes request at: ' + req.url);
 })
 
