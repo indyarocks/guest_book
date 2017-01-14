c76c169f7f23ecaae06bcab107d169340146daf5
Chandan Kumar <chandan.jhun@gmail.com>
2017-01-14 15:19:42 +0530

    express static middleware for static files

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 var express = require("express");
 var http = require("http");
-
+var logger = require("morgan");
+var path = require("path");
 var app = express();
 
-// New middleware
-app.use(function(req, res, next){
-	console.log('My fun middleware: start!')
-	console.log("Here comes a request " + req.method + " to " + req.url);
-	console.log('My fun middleware: end!')
-	next();
-});
+app.use(logger("short"));
+
+var publicPath = path.resolve(__dirname, "public");
+app.use(express.static(publicPath));
 
 app.use(function(req, res){
 	console.log('Here comes request at: ' + req.url);
 	res.writeHead(200, { "Content-Type": "text/plain"})
-	res.end("Here comes a request " + req.method + " to " + req.url);
+	res.end("Looks like no file in public directory!");
 	// res.status(200).send('Here comes request at: ' + req.url);
 })
 
